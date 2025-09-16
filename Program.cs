93ba6e6b6ae03acbaf2a9313d01ed0dafc2fdf12
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2025-09-16 11:35:40 +0200

    idk

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace kamennuzk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Zadej kamen, nuzky nebo papir: ");
+            string vstup = Console.ReadLine();
+
+            
+            Console.WriteLine("Zvolil si:" + vstup);
+        }
+    }
+}
